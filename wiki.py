d34f788b2da712db4dba99f7a85d29513afbfe3f
John Collins <177121194+old-doc-collins@users.noreply.github.com>
2024-12-20 01:27:29 +0000

    add error type

--- wiki.py
+++ wiki.py
@@ -1,10 +1,11 @@
 import wikipedia
+from wikipedia import PageError
 
 
 def scrape(name, length=1):
     try:
         result = wikipedia.summary(name, sentences=length)
-    except:
+    except PageError:
         result = None
     return result
 
