601a6659167de1feb11be2c445681530cd29e7b8
John Collins <177121194+old-doc-collins@users.noreply.github.com>
2024-12-29 21:51:51 +0000

    reverted

--- wiki.py
+++ wiki.py
@@ -1,15 +1,19 @@
-from mylib.wikistuff import scrape
+import wikipedia
+from wikipedia import PageError
 import click
 
 @click.command()
 @click.option('--name', prompt='entry', help='entry to look up.')
 @click.option('--length', default=1, help='number of sentences.')
-def run(name, length=1):
-    result = scrape(name, length)
+def scrape(name, length=1):
+    try:
+        result = wikipedia.summary(name, sentences=length)
+    except PageError:
+        result = None
     print(result)
 
 if __name__ == '__main__':
-    run()
+    scrape()
 
 
 
