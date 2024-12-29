f0482d881bdf2b9deed31a4667f11d2e07062019
John Collins <177121194+old-doc-collins@users.noreply.github.com>
2024-12-29 22:24:30 +0000

    patch

--- test_wiki.py
+++ test_wiki.py
@@ -1,7 +1,7 @@
 from wiki import scrape
 
 def test_scrape():
-    assert scrape('calculus') is not None
-    assert scrape('blargblumbpf') is None
+    assert scrape(name='calculus') is not None
+    assert scrape(name='blargblumbpf') is None
 
 
